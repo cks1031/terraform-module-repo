a60e590e7405966dcb8ad97fcde597d316c6b027
cks1031 <cks1031@naver.com>
2024-09-11 10:46:11 +0900

    terraform AWS EC2 instance FastAPI

---
+++ ec2_instance_fastapi/variables.tf
@@ -0,0 +1,20 @@
+variable "ami" {
+  description = "ami for instance"
+  type = string
+}
+variable "instance_type" {
+  description = "instance_type for instance"
+  type = string
+}
+variable "key_name" {
+  description = "key_name for instance"
+  type = string
+}
+variable "instance_name" {
+  description = "instance_name for instance"
+  type = string
+}
+variable "security_group_id" {
+  description = "id_sign for instance"
+  type = string
+}
