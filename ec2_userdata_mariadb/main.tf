6a43d012c78d8d1af47a7d847529787629b910fd
cks1031 <cks1031@naver.com>
2024-09-11 15:28:33 +0900

    Update main.tf

--- ec2_userdata_mariadb/main.tf
+++ ec2_userdata_mariadb/main.tf
@@ -2,14 +2,11 @@
 
 data "template_file" "mariadb_userdata" {
   template = <<-EOF
-    #!/bin/bash
-    set -e
 
     apt-get install -y apt-transport-https curl
-
     mkdir -p /etc/apt/keyrings
     curl -o /etc/apt/keyrings/mariadb-keyring.pgp 'https://mariadb.org/mariadb_release_signing_key.pgp'
-    
+
     cat <<EOL > /etc/apt/sources.list.d/mariadb.sources
     # MariaDB 10.11 repository list - created 2024-09-03 00:34 UTC
     # https://mariadb.org/download/
@@ -25,6 +22,7 @@ data "template_file" "mariadb_userdata" {
     apt-get install -y mariadb-server
 
     sudo mariadb-secure-installation <<EOF2
+
     y
     y
     ubuntu
